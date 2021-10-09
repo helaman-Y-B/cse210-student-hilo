1ff11966d97de63e12d452263bd3c38c7539f932
James Charlie Salva <JamesSalvajames@hotmail.com>
2021-10-10 02:12:01 +0800

    Updated and arrange it into a nice format, Remove some functions and if statements, and Add comments

--- hilo/game/dealer.py
+++ hilo/game/dealer.py
@@ -13,8 +13,6 @@ class Dealer:
         self.score = 300
         # calls the Dealer_card class.
         self.next_card = Dealer_card()
-        # declare old card variable to store the current card to compare it on the later part
-        self.old_card = 0
 
     def start_game(self):
         """Starts the game loop to control the sequence of play.
@@ -24,43 +22,37 @@ class Dealer:
         self.init_card = self.next_card.throw_card()
         self.current_card = ""
         self.final_score = 0
+
         while self.keep_playing:
             """Functions to start the game"""
-            # Calls the functions, making the game start
-            self.output(self.init_card)
-            #self.updates(self.init_card)
+            # Calls the output functions, making the game start
+            self.output(self.init_card) 
+
+            #Evaluating if the score is zero or not
             if self.score == 0:
-                print("GAME OVER")
-            play = input("do you want to play again? ")
-            if "n" in play:
+                #if the score is zero, it is game over and end of the game.
+                print("=============================================================================")
+                print("GAME OVER! \nThank you for you time playing this game. \nWe hope you have fun")
+                print("=============================================================================")
                 break
-            
-
-                    
-
-    def get_input(self):
-        """Gets the inputs at the beginning of each round of play. In this case,
-        that means the Dealer will show a new card.
-        Args:
-            self (Dealer): an instance of Dealer.
-        """
-        # Here is where the card will be stored, to be able to use it at the end of this file.
-        self.next_card.throw_card()
-        self.old_card = self.next_card.throw_card()
+            else:
+                #Else we will ask for the user if they wanted to play or not.
+                play = input("Keep Playing? [y/n]")
+                if "n" in play:
+                    print("Thank you for playing with us. Have a nice day!")
+                    break
         
-
     def updates(self, next_card):
         """Updates the important game information for each round of play. In 
         this case, that means updating the score.
         Args:
             self (Dealer): an instance of Dealer.
+            next_card: variable that store the next card for the game
+        Return:
+            final_score: return updated scores
         """
-        # calls the get_points function, and store  the points into a value called score
-        print(f"thisis the current card inside updates{self.current_card}")
-        print(f"thisis the current card inside updates{self.current_card}")
+        # calls the get_points function from Dealer_card class, and store  the points into a value called score
         points = self.next_card.get_points(next_card, self.current_card)
-        print(f" points eraned {points}")
-        print(f" current points {self.score}")
         final_score = self.score + points
         
         return final_score
@@ -70,21 +62,26 @@ class Dealer:
         this case, that means the card that were showed and the score.
         Args:
             self (Dealer): an instance of Dealer.
+            init_card: the inital card 
         """
-        # Print the current card and the current score.
-        #current_card = self.next_card.throw_card()
+        #storing card from the Dealer_card class to store as a next_card
         next_card = self.next_card.throw_card()
-
+        #Storing initial card as the current card for the comparison purposes
         self.current_card = init_card
-        print(f"\nThe Dealer Throw: {init_card}")
+        print(f"\nThe Card is: {init_card}")
 
-        if self.next_card.can_continue():
-            # The user will choose, if the card will be higher or lower then the last card.
-            choice = input("Will it be Heigher or Lower? [H/L] ").lower()
-            self.next_card.player_response = choice
-
-        print(f"Next Card: {next_card}")
+        #asking the player for his/her guess.
+        choice = input("Higher or Lower? [H/L] ").lower()
+        #getting the response of the player
+        self.next_card.player_response = choice
+        
+        #Showing the next card
+        print(f"Next Card was: {next_card}")
+        #Update of scores and storing the return of the update function.
         self.score = self.updates(next_card)
 
+        #printing of the current score
         print(f"Your current score is: {self.score}")
+
+        #Getting the value of next_card to be used as a parameter
         self.init_card = next_card
